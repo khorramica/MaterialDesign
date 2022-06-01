3d1042f546bc6d955a49763fec33ae66a7956fb3
khorramica <84560576+khorramica@users.noreply.github.com>
2022-06-01 09:45:46 +0430

    Initial commit

---
+++ app/src/main/java/ir/finto/materisaldesign/Part01/Login_Ebook_Activity.java
@@ -0,0 +1,16 @@
+package ir.finto.materisaldesign.Part01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.finto.materisaldesign.R;
+
+public class Login_Ebook_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ebook);
+    }
+}
